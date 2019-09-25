2e31efe35010728af80d0570627b70464f709f5f
Dmitri Zakharov <dmitri_zakharov@ftn.fedex.com>
2019-09-25 13:54:22 -0400

    Compile and run with local JavaFX 13 installation

--- chapter3/easytext-threemodules/run.sh
+++ chapter3/easytext-threemodules/run.sh
@@ -1,12 +1,20 @@
 mkdir -p out
 
-export JAVA_HOME=$JAVA11_FX_HOME
-
-$JAVA_HOME/bin/javac -d out --module-source-path src -m easytext.cli,easytext.gui,easytext.analysis
-
+# V1 - Use java distribution that already contains java-fx mods
+# export JAVA_HOME=$JAVA11_FX_HOME
+# $JAVA_HOME/bin/javac -d out --module-source-path src -m easytext.cli,easytext.gui,easytext.analysis
 # Run cli
-# $JAVA_HOME/bin/java --module-path out -m easytext.cli/javamodularity.easytext.cli.Main
 # $JAVA_HOME/bin/java --module-path out -m easytext.cli/javamodularity.easytext.cli.Main input.txt
-
 # Run gui
-$JAVA_HOME/bin/java --module-path out -m easytext.gui/javamodularity.easytext.gui.Main
+# $JAVA_HOME/bin/java --module-path out -m easytext.gui/javamodularity.easytext.gui.Main
+
+
+# V2 - # OR use the latest JavaFX 13 SDK and MODS installed locally.
+# Set the following environment variables in ~/.bashrc (~/.bash_profile)
+# export JAVA_FX=/home/ftnc/opt/javafx-sdk/lib
+# export JAVA_FX_MODS=/home/ftnc/opt/javafx-jmods
+export JAVA_HOME=$JAVA13_HOME
+# Specify path to JavaFX mods as they are not part of Java 13
+$JAVA_HOME/bin/javac --module-path $JAVA_FX_MODS -d out --module-source-path src -m easytext.cli,easytext.gui,easytext.analysis
+# Run gui - specify path to JavaFX SDK
+$JAVA_HOME/bin/java --module-path $JAVA_FX:out -m easytext.gui/javamodularity.easytext.gui.Main
