550bf7875c5a47384192236088885763a2aa7d39
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2019-09-24 23:21:32 -0400

    ch03 - A Tale of Two Modules

--- chapter3/easytext-twomodules/run.sh
+++ chapter3/easytext-twomodules/run.sh
@@ -1,4 +1,9 @@
 mkdir -p out
 
+export JAVA_HOME=$JAVA9_HOME
+
 $JAVA_HOME/bin/javac -d out --module-source-path src -m easytext.cli,easytext.analysis
-$JAVA_HOME/bin/java --module-path out -m easytext.cli/javamodularity.easytext.cli.Main
+# This works too, since easytext.cli requires easytext.analysis
+# $JAVA_HOME/bin/javac -d out --module-source-path src -m easytext.cli
+
+$JAVA_HOME/bin/java --module-path out -m easytext.cli/javamodularity.easytext.cli.Main input.txt
--- chapter3/easytext-twomodules/src/easytext.analysis/module-info.java
+++ chapter3/easytext-twomodules/src/easytext.analysis/module-info.java
@@ -1,3 +1,6 @@
 module easytext.analysis {
    exports javamodularity.easytext.analysis;
+
+   // cyclic dependence: causes compile error
+   // requires easytext.cli;
 }
