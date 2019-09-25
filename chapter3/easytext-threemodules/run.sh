8fd21804af1911b34ff34bcae787d89c86053a44
Dmitri Zakharov <dmitri_zakharov@ftn.fedex.com>
2019-09-25 10:30:50 -0400

    Configure JAVA11_FX_HOME and run gui

--- chapter3/easytext-threemodules/run.sh
+++ chapter3/easytext-threemodules/run.sh
@@ -1,4 +1,12 @@
 mkdir -p out
 
+export JAVA_HOME=$JAVA11_FX_HOME
+
 $JAVA_HOME/bin/javac -d out --module-source-path src -m easytext.cli,easytext.gui,easytext.analysis
-$JAVA_HOME/bin/java --module-path out -m easytext.cli/javamodularity.easytext.cli.Main
+
+# Run cli
+# $JAVA_HOME/bin/java --module-path out -m easytext.cli/javamodularity.easytext.cli.Main
+# $JAVA_HOME/bin/java --module-path out -m easytext.cli/javamodularity.easytext.cli.Main input.txt
+
+# Run gui
+$JAVA_HOME/bin/java --module-path out -m easytext.gui/javamodularity.easytext.gui.Main
